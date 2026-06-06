079cce660af2f82e9ff7a2f9c7b8cf611cb65a49
abderrahim hamdi <elhamdiabderrahim8@gmail.com>
2026-06-06 18:21:14 +0200

    fix: connect APK to Render.com backend instead of localhost

--- flutter_application_1/lib/utils/global_config.dart
+++ flutter_application_1/lib/utils/global_config.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
+
 class GlobalConfig {
-  /// URL de base vers le serveur Flask local.
-  /// Si vous testez sur un vrai téléphone Android (Infinix), utilisez l'IP locale (ex: 'http://192.168.1.16:5000').
-  /// Si vous testez sur l'émulateur Android, utilisez 'http://10.0.2.2:5000'.
-  static const String backendUrl = 'http://192.168.1.16:5000';
+  /// URL du backend.
+  /// - Production : Render.com (utilisé dans l'APK déployé)
+  /// - Développement : IP locale (pour le debug sur votre réseau Wi-Fi)
+  static const String backendUrl = kReleaseMode
+      ? 'https://mundialy-backend.onrender.com'
+      : 'http://192.168.1.16:5000';
 }
